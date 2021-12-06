4125ad0adfc5a61997304ae2cbdb10a1687e468c
Viktor Kassov <kassov@mail.ua>
2021-12-06 17:30:42 +0200

    3/6+

--- Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric Matrix Processor/task/src/processor/Main.kt
@@ -1,39 +1,94 @@
 package processor
 
+import java.math.BigDecimal
+
 fun main() {
+    menu()
+}
+
+fun menu() {
+    while (true) {
+        println("1. Add matrices")
+        println("2. Multiply matrix by a constant")
+        println("3. Multiply matrices")
+        println("0. Exit")
+        when (readLine()!!) {
+            "1" -> addMatrices()
+            "2" -> multiplyByConst()
+            "3" -> multiplyMatrices()
+            "0" -> return
+        }
+    }
+
+}
+
+fun multiplyMatrices() {
+    println("Enter size of first matrix:")
     val size1 = readLine()!!.split(" ")
     val matrix1 = Matrix(size1[0].toInt(), size1[1].toInt())
+    println("Enter first matrix:")
     matrix1.fill()
-    val mult = readLine()!!.toInt()
+    println("Enter size of second matrix:")
+    val size2 = readLine()!!.split(" ")
+    val matrix2 = Matrix(size2[0].toInt(), size2[1].toInt())
+    println("Enter second matrix:")
+    matrix2.fill()
+    try {
+        val matrix3 = matrix1 * matrix2
+        println("The result is:")
+        matrix3.print()
+    } catch(e: Exception) {
+        println("The operation cannot be performed.")
+    }
+}
+
+fun multiplyByConst() {
+    println("Enter size of first matrix:")
+    val size1 = readLine()!!.split(" ")
+    val matrix1 = Matrix(size1[0].toInt(), size1[1].toInt())
+    println("Enter first matrix:")
+    matrix1.fill()
+    println("Enter constant:")
+    val mult = readLine()!!.toBigDecimal()
     val result = matrix1 * mult
+    println("The result is:")
     result.print()
-//    val size2 = readLine()!!.split(" ")
-//    val matrix2 = Matrix(size2[0].toInt(), size2[1].toInt())
-//    matrix2.fill()
+}
 
-//    try {
-//        val matrix3 = matrix1 + matrix2
-//        matrix3.print()
-//    } catch(e: Exception) {
-//        println("ERROR")
-//    }
-
+fun addMatrices() {
+    println("Enter size of first matrix:")
+    val size1 = readLine()!!.split(" ")
+    val matrix1 = Matrix(size1[0].toInt(), size1[1].toInt())
+    println("Enter first matrix:")
+    matrix1.fill()
+    println("Enter size of second matrix:")
+    val size2 = readLine()!!.split(" ")
+    val matrix2 = Matrix(size2[0].toInt(), size2[1].toInt())
+    println("Enter second matrix:")
+    matrix2.fill()
+    try {
+        val matrix3 = matrix1 + matrix2
+        println("The result is:")
+        matrix3.print()
+    } catch(e: Exception) {
+        println("The operation cannot be performed.")
+    }
 }
 
 class Matrix(_rows: Int, _cols: Int ) {
     val rows = _rows
     val cols = _cols
-    val array: MutableList<MutableList<Int>> = mutableListOf<MutableList<Int>>()
+    val array: MutableList<MutableList<BigDecimal>> = mutableListOf<MutableList<BigDecimal>>()
     init {
         for (i in 0 until rows) {
-            this.array.add(MutableList<Int>(cols){ 0 })
+            this.array.add(MutableList<BigDecimal>(cols){ 0.toBigDecimal() })
         }
     }
     fun fill() {
         for (i in 0 until rows) {
             val rowArray = readLine()!!.split(" ")
             for (j in 0 until cols) {
-                array[i][j] = rowArray[j].toInt()
+                array[i][j] = rowArray[j].toBigDecimal()
             }
         }
     }
@@ -51,7 +106,21 @@ class Matrix(_rows: Int, _cols: Int ) {
         }
     }
 
-    operator fun times(mult: Int): Matrix {
+    operator fun times(matrix: Matrix): Matrix {
+        if (this.cols == matrix.rows) {
+            val result = Matrix(this.rows, matrix.cols)
+            for (i in 0 until result.rows) {
+                for (j in 0 until result.cols) {
+                    result.array[i][j] = this.array[i].foldIndexed(0.toBigDecimal()) { index, acc, v -> acc + v * matrix.array[index][j] }
+                }
+            }
+            return result
+        } else {
+            throw Exception("IncorrectSize")
+        }
+    }
+
+    operator fun times(mult: BigDecimal): Matrix {
         val result = Matrix(this.rows, this.cols)
         for (i in 0 until rows) {
             for (j in 0 until cols) {
